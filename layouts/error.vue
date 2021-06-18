4a5215221ff3440587c3c4ee63592d25a4a09f32
Laurens Verspeek <l.verspeek@gmail.com>
2021-06-18 11:05:22 +0200

    add bulma

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="has-text-centered mt-6">
+    <h1 class="title" v-if="error.statusCode === 404">Page not found</h1>
+    <h1 class="title" v-else>An error occurred</h1>
+    <nuxt-link to="/" class="button">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['error'],
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  }
+}
+</script>
