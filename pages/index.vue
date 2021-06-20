00f61477bb8af2af3afb691c148dabb20536fe93
Laurens Verspeek <laurens_verspeek@hotmail.com>
2021-06-20 18:14:13 +0200

    add content homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
   <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Nosana CI
-      </h1>
+    <div class="container ">
+      <div class="columns is-vcentered" style="min-height: 65vh;">
+        <div class="column">
+          <h1 class="title is-spaced site-title">
+            Nosana CI
+          </h1>
+          <h2 class="subtitle">
+            A decentralized CI/CD solution on the Solana blockchain.
+          </h2>
+          <div class="columns">
+            <p class="column is-5">
+              Nostromo leverages the Proof-of-History consensus of the Solana Network to build a decentralized continuous integration, delivery, and deployment platform for open source projects.
+            </p>
+          </div>
+          <div>
+            <div class="button is-secondary is-wide has-tooltip-bottom has-tooltip-arrow" data-tooltip="Coming Soon!">
+              Demo
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -15,4 +32,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.site-title {
+  font-size: 60px;
+}
 </style>
