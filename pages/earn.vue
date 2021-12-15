f3ac2069f3690cb2f37a467de4d5219dcba0689b
Laurens Verspeek <l.verspeek@gmail.com>
2021-12-15 13:44:06 +0100

    WIP add earn page

---
+++ pages/earn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-5">
+            <h1 class="title site-title">
+              Earn <span class="has-text-accent">NOS</span> tokens
+            </h1>
+            <p>
+              Provide your unused computing capacity to promising projects in the Metaverse and get paid in NOS.
+            </p>
+          </div>
+          <div class="column has-text-centered">
+            <div class="mt-6 pt-6">
+              <img src="~assets/img/nosana-token.svg" width="456">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  colorMode: 'dark'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
