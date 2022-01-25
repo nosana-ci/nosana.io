1dcab8478206d5e88e73e08ff3ce2adebbb886ae
Laurens Verspeek <l.verspeek@gmail.com>
2022-01-25 12:22:48 +0100

    update roadmap

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="roadmap-container pb-6 my-6">
-    <h2 class="title has-text-centered">
-      Roadmap
-    </h2>
+    <h2 class="title has-text-centered">Roadmap</h2>
     <div class="roadmap py-6 mb-6">
-      <div v-for="item in roadmap" :key="item.milestone" :class="'roadmap-item ' + item.class">
-        <atropos :options="{highlight: false, rotateYMax: 0, rotateTouch: false}" class="my-atropos">
+      <div
+        v-for="item in roadmap"
+        :key="item.milestone"
+        :class="'roadmap-item ' + item.class"
+      >
+        <atropos
+          :options="{ highlight: false, rotateYMax: 0, rotateTouch: false }"
+          class="my-atropos"
+        >
           <div class="quarter-block has-radius-small p-3">
             <div class="quarter">
               {{ item.date }}
-              <span class="is-pulled-right is-size-6">{{ item.year ? item.year : 2022 }}</span>
+              <span class="is-pulled-right is-size-6">{{
+                item.year ? item.year : 2022
+              }}</span>
             </div>
             <div v-html="item.milestone" />
             <ul v-if="item.points" class="is-hidden-desktop">
@@ -30,14 +37,14 @@
 </template>
 
 <script>
-
 export default {
   data () {
     return {
       roadmap: [
         {
           date: '1.',
-          milestone: 'Complete demo for Solana Foundation & finalize release strategy for the incentivized Testnet and Mainnet',
+          milestone:
+            'Complete demo for Solana Foundation & finalize release strategy for the incentivized Testnet and Mainnet',
           class: 'is-completed',
           year: 'Done'
         },
@@ -49,15 +56,17 @@ export default {
         },
         {
           date: '3.',
-          milestone: 'Complete the Incentivised Testnet - mining & staking module',
-          class: 'is-active',
-          year: ' '
+          milestone:
+            'Complete Smart Contract security audit, conduct <b>Solanium Public Sale</b>, and distribute $NOS tokens',
+          class: 'is-completed',
+          year: 'Done'
         },
         {
           date: '4.',
-          milestone: 'Complete Smart Contract security audit, conduct <b>Solanium Public Sale</b>, and distribute $NOS tokens',
-          class: '',
-          year: '17-1'
+          milestone:
+            'Complete the Incentivised Testnet - mining & staking module',
+          class: 'is-active',
+          year: ' '
         },
         {
           date: '5.',
@@ -87,12 +96,11 @@ export default {
     }
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
 .roadmap-container {
-  background: url('~assets/img/field.svg') no-repeat;
+  background: url("~assets/img/field.svg") no-repeat;
   background-position: bottom center;
   background-size: contain;
 }
@@ -112,7 +120,7 @@ export default {
     content: "";
     display: block;
     position: absolute;
-    left:-2px;
+    left: -2px;
     width: 2px;
     height: 100%;
     background: $accent;
@@ -132,12 +140,20 @@ export default {
 
   &:first-child {
     &:before {
-      background: linear-gradient(180deg, rgba($accent, 0) 0%, rgba($accent, 1) 40%);
+      background: linear-gradient(
+        180deg,
+        rgba($accent, 0) 0%,
+        rgba($accent, 1) 40%
+      );
     }
   }
   &:last-child {
     &:before {
-      background: linear-gradient(180deg, rgba($accent, 1) 40%, rgba($accent, 0) 100%);
+      background: linear-gradient(
+        180deg,
+        rgba($accent, 1) 40%,
+        rgba($accent, 0) 100%
+      );
     }
   }
   ul {
@@ -147,7 +163,7 @@ export default {
     font-weight: 300;
     li {
       &:before {
-        content: "\2022";  /* Add content: \2022 is the CSS Code/unicode for a bullet */
+        content: "\2022"; /* Add content: \2022 is the CSS Code/unicode for a bullet */
         color: white; /* Change the color */
         font-weight: bold; /* If you want it to be bold */
         font-size: 14px;
@@ -213,12 +229,11 @@ export default {
   }
   @-webkit-keyframes glow-box {
     from {
-      box-shadow: 0 0 7px 0 rgba($accent,0.4);
+      box-shadow: 0 0 7px 0 rgba($accent, 0.4);
     }
     to {
-      box-shadow: 0 0 17px 8px rgba($accent,0.6);
+      box-shadow: 0 0 17px 8px rgba($accent, 0.6);
     }
-
   }
   &.is-active {
     &:after {
@@ -234,7 +249,8 @@ export default {
       border-color: $accent;
       animation: glow-box 1s ease-in-out infinite alternate;
       color: black;
-      &:before, &:after {
+      &:before,
+      &:after {
         background: $accent;
       }
     }
@@ -248,5 +264,4 @@ export default {
     }
   }
 }
-
 </style>
