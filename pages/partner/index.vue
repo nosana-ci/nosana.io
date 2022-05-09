91524aeab58df10e3bdd801c1c50a9e6a20009c5
Laurens Verspeek <l.verspeek@gmail.com>
2022-05-09 15:51:36 +0200

    WIP: partner wireframe

---
+++ pages/partner/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="columns mt-6">
+          <div class="column is-5" data-aos="fade-up">
+            <h1 class="title site-title is-spaced">
+              Nosana Partner Program
+            </h1>
+            <h2 class="subtitle">
+              Join our growing community
+              Integrate your solution with Nosana Network.
+            </h2>
+            <div class="has-limited-width mt-4">
+              <p class="block">
+                <a
+                  href="#"
+                  target="_blank"
+                  class="button is-accent is-wide"
+                >
+                  <b>Become a partner</b>
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="is-offset-1 column has-text-centered">
+            <div>
+              <img src="~assets/img/bounty_animated.svg">
+            </div>
+          </div>
+        </div>
+        <div class="my-6 pt-6 has-text-centered">
+          <h2 class="title mb-6" data-aos="fade-right">
+            A new opportunity to<br>grow your customer base
+          </h2>
+          <div class="columns is-variable is-8">
+            <div class="column is-one-third" data-aos="fade-up" data-aos-delay="200">
+              <img src="~assets/img/icons/token.svg" height="80">
+              <p class="has-limited-width-smaller is-horizontal-centered">
+                Reach our growing crypto-communities with our go-to-market initiatives.
+              </p>
+            </div>
+            <div class="column is-one-third" data-aos="fade-up" data-aos-delay="400">
+              <img src="~assets/img/icons/apps.svg" height="80">
+              <p class="has-limited-width-smaller is-horizontal-centered">
+                Build your expertise in continuous integration and delivery with technical support from Nosana.
+              </p>
+            </div>
+            <div class="column is-one-third" data-aos="fade-up" data-aos-delay="600">
+              <img src="~assets/img/icons/badge.svg" height="80">
+              <p class="has-limited-width-smaller is-horizontal-centered">
+                Work with Nosana to help your clients achieve new levels of DevOps maturity.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="columns pt-6 is-vcentered">
+          <div class="column is-5 has-background-gradient" data-aos="fade-left">
+            <h2 class="title is-2">
+              <span class="has-text-accent">Technology</span> Partners
+            </h2>
+            <p class="block">
+              Make it easy for your customers to integrate your toold with their CI/CD pipeline,
+              get extra exposure to other Nosana customers as we automate their DevOps processes.
+              Be part of our growing network of partner companies.
+            </p>
+            <p class="block">
+              <a href="#" class="mt-4"><b>Become a Technology Partner &nbsp;&nbsp;↘</b></a>
+              <nuxt-link to="/partner/technology" class="mt-4 ml-6">
+                <b>More info &nbsp;&nbsp;↘</b>
+              </nuxt-link>
+            </p>
+          </div>
+          <div class="column is-5 is-offset-2" data-aos="zoom-in">
+            <img src="~assets/img/tokens.png">
+          </div>
+        </div>
+        <div class="columns is-vcentered">
+          <div class="column is-5" data-aos="zoom-in">
+            <img src="~assets/img/tokens.png">
+          </div>
+          <div class="column is-offset-2 is-5  has-background-gradient" data-aos="fade-left">
+            <h2 class="title is-2">
+              <span class="has-text-accent">Network</span> Partners
+            </h2>
+            <p class="block">
+              Open up new dev opportunities for your company by partnering
+              with the first decentralized CICD as a Service provider.
+              Network Partners need to have open-source & public repositories.
+              Improve the level of automation, quality of software,
+              developer experience and the frequency of delivery with tailored-made pipelines.
+              We help you get the most out of your development.
+            </p>
+            <p class="block">
+              <a href="#" class="mt-4"><b>Become a Network Partner &nbsp;&nbsp;↘</b></a>
+              <nuxt-link to="/partner/technology" class="mt-4 ml-6">
+                <b>More info &nbsp;&nbsp;↘</b>
+              </nuxt-link>
+            </p>
+          </div>
+        </div>
+        <div class="columns is-vcentered">
+          <div class="column is-5  has-background-gradient" data-aos="fade-left">
+            <h2 class="title is-2">
+              <span class="has-text-accent">Portfolio</span> Partners
+            </h2>
+            <p class="block">
+              The Nosana Portfolio program allows VCs and accelerators to offer Nosana as an
+              exclusive benefit to startups in their ecosystem.
+            </p>
+            <p class="block">
+              <a href="#" class="mt-4"><b>Become a Portfolio Partner &nbsp;&nbsp;↘</b></a>
+              <nuxt-link to="/partner/technology" class="mt-4 ml-6">
+                <b>More info &nbsp;&nbsp;↘</b>
+              </nuxt-link>
+            </p>
+          </div>
+          <div class="column is-5 is-offset-2" data-aos="zoom-in">
+            <img src="~assets/img/tokens.png">
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  colorMode: 'dark'
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
