4d9528cc8d09862b8b94a523202b93187c247209
Laurens Verspeek <l.verspeek@gmail.com>
2022-07-15 16:44:29 +0200

    add nft page

---
+++ pages/nft.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="columns mt-5">
+          <div class="column is-6" data-aos="fade-up">
+            <h1 class="title site-title is-spaced has-limited-width mt-6">
+              Meet our Burner Phone NFT collection
+            </h1>
+            <div class="has-limited-width-small mt-4">
+              <p class="block">
+                You’re probably familiar with NFTs – you know, unique digital collectibles.
+                Now, we’ve got our very own collection,
+                consisting of 850 Burner Phone NTFs that exist on the Solana Blockchain.
+                Think of a Burner Phone NFT as your very own Network membership card.
+                It even comes with owner-only perks,
+                which you can access on the Nosana Network, the Metaverse or at home.
+              </p>
+              <p class="block">
+                <a
+                  disabled
+                  target="_blank"
+                  class="mr-3 button is-accent is-wide is-disabled"
+                >
+                  NFT Machine coming soon
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="column has-text-centered mb-6">
+            <div>
+              <img src="~assets/img/burner-phone.png">
+            </div>
+          </div>
+        </div>
+        <div class="my-6 py-6 has-text-centered has-background-gradient">
+          <h2 class="title is-2 mb-6" data-aos="fade-right">
+            Get yours for<br><span class="has-text-accent">0 SOL</span>
+          </h2>
+          <p class="has-limited-width-small is-horizontal-centered" data-aos="fade-up">
+            We don’t do minting fees – a Burner Phone costs exactly 0 SOL.
+            Are you a Nosana Network user? Stake $NOS for a chance to get your very own.
+          </p>
+          <div class="has-background-accent is-horizontal-centered my-5" style="width: 80px; height: 2px" />
+          <p>
+            <a disabled class="is-disabled is-accent button is-outlined">Staking coming soon</a>
+          </p>
+        </div>
+        <div class="has-text-centered">
+          <h2 class="title" data-aos="fade-right">
+            Good to know
+          </h2>
+          <p class="has-limited-width-small is-horizontal-centered" data-aos="fade-up">
+            We’re holding on to 100 Burner Phones.
+            They’ll be used for giveaways, rewards and the creators’ Nosana Network memberships.
+          </p>
+        </div>
+        <div class="columns py-6 is-vcentered mt-6">
+          <div class="column is-7" data-aos="zoom-in">
+            <img src="~assets/img/burner-phone-logo.png">
+          </div>
+          <div class="column is-5" data-aos="fade-left">
+            <h2 class="title is-2">
+              Why a Burner Phone?
+            </h2>
+            <p class="block">
+              The cell phone is an object so entwined with our daily lives,
+              we thought it deserved its own metaverse counterpart.
+              Our NFT isn’t just any cell phone, though, it’s a burner phone.
+              An off-the-grid, disposable item that represents anonymity.
+              Perfect for those who dream of a decentralized future – like us.
+            </p>
+          </div>
+        </div>
+        <div class="columns  is-vcentered">
+          <div class="column is-5" data-aos="fade-right">
+            <h2 class="title is-2">
+              Coming to a street near you
+            </h2>
+            <p class="block">
+              Our Burner Phone vending machines will soon hit the (digital) streets.
+              To celebrate, we’ll be giving away 50 credits – which you can use to buy a Burner Phone.
+              Keep an eye out for more details on our very first community giveaway.
+            </p>
+          </div>
+          <div class="column is-5 is-offset-2" data-aos="zoom-in">
+            <img src="~assets/img/nos-machine.png">
+          </div>
+        </div>
+        <div class="my-6 pt-6 has-text-centered">
+          <h2 class="title is-2" data-aos="fade-up">
+            Not just a JPEG
+          </h2>
+          <p class="has-limited-width-small is-horizontal-centered" data-aos="fade-up">
+            Burner Phone NFTs aren’t ‘just JPEGs’ –
+            they’re powerful assets you can use within the Network. Not to mention all the perks that come with them.
+          </p>
+        </div>
+        <div class="columns py-6 is-vcentered mt-6">
+          <div class="column is-7" data-aos="zoom-in">
+            <img src="~assets/img/burner-phones.png">
+          </div>
+          <div class="column is-5" data-aos="fade-left">
+            <h2 class="title">
+              Perks for Burner<br> Phone owners:
+            </h2>
+            <p class="block">
+              <ul>
+                <li>Guaranteed early access to our Incentivized Testnet during phase 2</li>
+                <li>Access to an exclusive Discord channel where you can vote on future rewards and new properties</li>
+                <li>Free virtual representation of your NFT, to be used in future metaverses</li>
+                <li>3D-printed figurine of your NFT</li>
+                <li>Upgrade to a 3D Burner Phone model in the future</li>
+                <li>Access to exclusive merch throughout the year – Burner Phones with certain properties only</li>
+                <li>
+                  One-of-a-kind reward-boosting utility when mainnet goes live
+                  – for a limited number of Burner Phones only, details to come
+                </li>
+                <li>Access to the Nosana workbench – details to come</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div class="my-6 pt-6 has-text-centered">
+          <h2 class="title" data-aos="fade-up">
+            Special Burner Phone Network features
+          </h2>
+        </div>
+        <div class="columns is-variable is-8">
+          <div class="column is-one-third" data-aos="fade-up" data-aos-delay="200">
+            <div class="has-background-gradient  has-border-accent px-4 py-5">
+              <h2 class="title is-4 has-text-accent">
+                Staking
+              </h2>
+              <p>
+                <ul>
+                  <li class="bronze">
+                    Bronze: get 5% extra $CPU when you stake the NFT
+                  </li>
+                  <li class="silver">
+                    Silver: get 10% extra $CPU when you stake the NFT
+                  </li>
+                  <li class="gold">
+                    Gold: get 15% extra $CPU when you stake the NFT
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div class="column is-one-third " data-aos="fade-up" data-aos-delay="400">
+            <div class="has-background-gradient has-border-accent px-4 py-5">
+              <h2 class="title is-4 has-text-accent">
+                Earning NOS
+              </h2>
+              <p>
+                <ul>
+                  <li class="bronze">
+                    Bronze: earn 5% extra $NOS with each pipeline
+                  </li>
+                  <li class="silver">
+                    Silver: earn 10% extra $NOS with each pipeline
+                  </li>
+                  <li class="gold">
+                    Gold: earn 15% extra $NOS with each pipeline
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div class="column is-one-third " data-aos="fade-up" data-aos-delay="600">
+            <div class="has-background-gradient has-border-accent px-4 py-5">
+              <h2 class="title is-4 has-text-accent">
+                Discount NOS
+              </h2>
+              <p>
+                <ul>
+                  <li class="bronze">
+                    Bronze: 5% discount $NOS with each pipeline
+                  </li>
+                  <li class="silver">
+                    Silver: 10% discount $NOS with each pipeline
+                  </li>
+                  <li class="gold">
+                    Gold: 15% discount $NOS with each pipeline
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="columns mt-6 is-vcentered">
+          <div class="column is-7">
+            <img src="~assets/img/tower_animated.svg">
+          </div>
+          <div class="column is-5">
+            <roadmap-nft />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  colorMode: 'dark'
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  margin: 20px;
+  li {
+    &:before {
+      content: "\2022"; /* Add content: \2022 is the CSS Code/unicode for a bullet */
+      color: $accent; /* Change the color */
+      font-weight: bold; /* If you want it to be bold */
+      font-size: 14px;
+      display: inline-block; /* Needed to add space between the bullet and the text */
+      width: 1em; /* Also needed for space (tweak if needed) */
+      margin-left: -1em; /* Also needed for space (tweak if needed) */
+    }
+    &.bronze:before {
+      content: "";
+      background: #F2994A;
+      height: 8px;
+      width: 8px;
+      margin-right: 8px;
+    }
+    &.silver:before {
+      content: "";
+      background: #E8E8E8;
+      height: 8px;
+      width: 8px;
+      margin-right: 8px;
+    }
+    &.gold:before {
+      content: "";
+      background: #F2C94C;
+      height: 8px;
+      width: 8px;
+      margin-right: 8px;
+    }
+  }
+}
+</style>
