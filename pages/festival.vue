e3d6157a99ea260e37393c096c68c957a1fb57a7
Jairzijp <jairzijp123@gmail.com>
2022-10-18 17:51:29 +0200

    improvements festival page

--- pages/festival.vue
+++ pages/festival.vue
@@ -3,8 +3,8 @@
     <section class="section">
       <div class="container">
         <!-- Intro -->
-        <div class="columns mt-5">
-          <div class="column is-6" data-aos="fade-up">
+        <div class="columns mt-6 mb-6 pb-6 intro">
+          <div class="column is-6 is-offset-1" data-aos="fade-up">
             <h1 class="title site-title is-spaced has-limited-width mt-6" data-aos="fade-up">
               Welcome to the first Burner Phones Festival!
             </h1>
@@ -24,7 +24,7 @@
               </p>
             </div>
           </div>
-          <div class="column has-text-centered mb-6">
+          <div class="column has-text-centered is-4 mb-6">
             <div>
               <img src="~assets/img/festival/bpf.png">
             </div>
@@ -32,7 +32,7 @@
         </div>
         <!-- Lootboxes -->
         <div class="box p-6 has-border-accent mt-6">
-          <div class="is-pulled-right">
+          <div class="is-pulled-right" data-aos="fade-left" data-aos-delay="400">
             <span>Results every day</span>
             <div class="is-flex">
               <p class="mr-3">
@@ -49,49 +49,49 @@
               </p>
             </div>
           </div>
-          <h1 class="title">
+          <h1 class="title" data-aos="fade-up" data-aos-delay="200">
             Lootbox Drop incoming!
           </h1>
-          <p class="has-limited-width-small">
+          <p class="has-limited-width-small" data-aos="fade-in">
             Wait for that call with our droplocation to come in. We have four different drops during the festival.
             The price of the day will be shared every morning at 10:00 CET.
             The winner will be published around 20:00 CET.
           </p>
           <div class="columns lootboxes mt-5">
-            <div class="column is-3 has-text-centered">
+            <div class="column is-3 has-text-centered" data-aos="fade-up" data-aos-delay="200">
               <img src="~assets/img/festival/lootbox-regular.png">
               <span>Regular drop</span>
             </div>
-            <div class="column is-3 has-text-centered">
+            <div class="column is-3 has-text-centered" data-aos="fade-up" data-aos-delay="400">
               <img src="~assets/img/festival/lootbox-nos.png">
               <span>NOS drop</span>
             </div>
-            <div class="column is-3 has-text-centered">
+            <div class="column is-3 has-text-centered" data-aos="fade-up" data-aos-delay="600">
               <img src="~assets/img/festival/lootbox-rare.png">
               <span>Rare drop</span>
             </div>
-            <div class="column is-3 has-text-centered">
+            <div class="column is-3 has-text-centered" data-aos="fade-up" data-aos-delay="800">
               <img src="~assets/img/festival/lootbox-nos.png">
               <span>Special drop</span>
             </div>
           </div>
         </div>
-
-        <!-- Prizes carousel -->
-        <div class="my-6 pt-6 has-text-centered">
-          <h2 class="title" data-aos="fade-up">
-            Are you ready for the <br> hot end of summer?
-          </h2>
-          <p class="has-limited-width-small is-horizontal-centered" data-aos="fade-up">
-            Take a quick look at our amazing prizepool.
-            Are you the lucky holder to win a Ipad Air, Xbox Series X, Go Pro Hero 4 or one of the many other prizes?!
-          </p>
-        </div>
+      </div>
+      <!-- Prizes carousel -->
+      <div class="my-6 pt-6 has-text-centered">
+        <h2 class="title" data-aos="fade-up">
+          Are you ready for the <br> hot end of summer?
+        </h2>
+        <p class="has-limited-width-small is-horizontal-centered" data-aos="fade-up">
+          Take a quick look at our amazing prizepool.
+          Are you the lucky holder to win a Ipad Air, Xbox Series X, Go Pro Hero 4 or one of the many other prizes?!
+        </p>
       </div>
       <client-only>
         <carousel-3d
           v-if="slides"
           ref="carousel"
+          class="mb-6"
           :autoplay="true"
           :width="320"
           :height="275"
@@ -99,8 +99,7 @@
           :space="330"
           :clickable="false"
           :controls-visible="false"
-          :autoplay-timeout="4000"
-          :animation-speed="500"
+          :autoplay-timeout="3000"
         >
           <slide
             v-for="(slide, index) in slides"
@@ -112,6 +111,38 @@
           </slide>
         </carousel-3d>
       </client-only>
+      <div class="container mb-6 pt-6">
+        <div class="column is-8 is-offset-2 box has-border-accent has-background-light mt-6">
+          <div class="columns">
+            <div class="column is-5 p-6">
+              <img
+                src="~assets/img/festival/phone.png"
+                class="p-5"
+                style="width: 100%;"
+                data-aos="zoom-in"
+                data-aos-delay="200"
+              >
+            </div>
+            <div
+              class="column is-7 p-5
+                is-flex is-flex-direction-column is-align-content-center is-justify-content-center"
+              data-aos="fade-up"
+            >
+              <h2 class="title">
+                Only for Burner Phones holders
+              </h2>
+              <p>This is a Burner Phones holders event only. Do you wanna get your hand on a BurnerPhone?</p>
+              <br>
+              <p>
+                <a href="https://app.nosana.io/stake" target="_blank" class="has-text-accent">Stake</a> your NOS tokens and
+                <a href="https://app.nosana.io/stake" target="_blank" class="has-text-accent">Subscribe</a> for the next free mint on Nosana's Network.
+                Or take a look on Solana Marketplaces like <a href="https://magiceden.io/marketplace/nosana_burner_phone" target="_blank" class="has-text-accent">Magic Eden</a> and
+                <a target="_blank" href="https://opensea.io/collection/nosana-burner-phone-collection" class="has-text-accent">OpenSea</a>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -125,7 +156,7 @@ export default {
         'gopro.png',
         'xbox.png',
         'ipad.png',
-        'gopro.png',
+        'apple.png',
         'sonos.png',
         'ledger.png'
       ]
@@ -149,6 +180,15 @@ export default {
   img {
     height: 100%;
     object-fit: contain;
+  }
+}
+@media screen and (max-width: $tablet) {
+  .intro {
+    margin-top: 0 !important;
+  }
+  .is-pulled-right {
+    float: none !important;
+    margin-bottom: 20px;
   }
 }
 </style>
