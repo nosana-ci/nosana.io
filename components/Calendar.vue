afd93bd50472374f24072ba222e5638b5c70343c
Laurens Verspeek <l.verspeek@gmail.com>
2022-10-18 17:01:17 +0200

    WIP calendar

---
+++ components/Calendar.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="table-container">
+        <table class="table is-bordered">
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+              <th>Sunday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="week in 2" :key="week">
+              <td
+                v-for="day in 7"
+                :key="day"
+                :class="{'is-active': currentDay === (day + 28 + 7*(week-1))%31 + 1,
+                         'is-selected': prizes && prizes[(day + 28 + 7*(week-1))%31 + 1].prize}"
+              >
+                {{ (day + 28 + 7*(week-1))%31 + 1 }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    // const now = new Date();
+    return {
+      prizes: {
+        30: { type: 'normal', prize: 1 },
+        31: { type: 'normal', prize: 2 },
+        1: { type: 'normal', prize: 3 },
+        2: { type: 'normal', prize: 4 },
+        3: { type: 'normal', prize: 5 },
+        4: { type: 'normal', prize: null },
+        5: { type: 'normal', prize: null },
+        6: { type: 'normal', prize: null },
+        7: { type: 'normal', prize: null },
+        8: { type: 'normal', prize: null },
+        9: { type: 'normal', prize: null },
+        10: { type: 'normal', prize: null },
+        11: { type: 'normal', prize: null },
+        12: { type: 'normal', prize: null }
+      },
+      currentDay: 3
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+th {
+  border-color: $grey;
+}
+td {
+  min-width: 156px;
+  height: 130px;
+  border-color: $grey;
+}
+td.is-active {
+  border-color: $accent;
+  border-width: 2px;
+}
+</style>
