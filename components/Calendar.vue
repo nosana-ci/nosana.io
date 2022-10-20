bafe4999e0aa0f6ae31791aeda74f4c3377063d0
Jairzijp <jairzijp123@gmail.com>
2022-10-20 15:10:42 +0200

    setup design winners block, but hide for now

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,62 +1,95 @@
 <template>
-  <div>
-    <div class="">
-      <h4 class="subtitle has-text-accent" data-aos="fade-up">
-        <span class="has-text-weight-bold">Calendar</span>
-        <span style="padding-left:100px">
-          day: <span
-            v-if="prizes"
-            class="has-text-weight-bold"
-          >{{ (prizes.filter(p => p.prize)).length }}</span><span class="has-text-weight-bold">/10</span></span>
-      </h4>
-      <h2 class="title" data-aos="fade-up">
-        The Burner Phones<br>Festival
-      </h2>
-      <h4 class="mt-6 mb-4 subtitle has-text-accent has-text-centered has-text-weight-bold mb-3" data-aos="fade-up">
-        October 30 – November 12
-      </h4>
+  <div :class="{'columns': winners}">
+    <div :class="{'column is-10': winners}">
+      <div class="">
+        <h4 class="subtitle has-text-accent is-family-sans-serif" data-aos="fade-up">
+          <span class="has-text-weight-bold">Calendar</span>
+          <span style="padding-left:100px">
+            day: <span
+              v-if="prizes"
+              class="has-text-weight-bold"
+            >{{ (prizes.filter(p => p.prize)).length }}</span><span class="has-text-weight-bold">/10</span></span>
+        </h4>
+        <h2 class="title" data-aos="fade-up">
+          The Burner Phones<br>Festival
+        </h2>
+        <h4
+          class="mt-6 mb-4 subtitle is-family-sans-serif has-text-accent
+          has-text-centered has-text-weight-bold"
+          data-aos="fade-up"
+        >
+          November 1 – November 10
+        </h4>
+      </div>
+      <div class="table-container">
+        <table class="table is-bordered">
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+              <th>Sunday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="week in 2" :key="week">
+              <td
+                v-for="prize in prizes.slice((week-1) * 7,week * 7)"
+                :key="prize.day"
+                :data-tooltip="
+                  prize.info ? prize.info : null"
+                :class="{'is-active': currentDay === prize.day,
+                         'is-selected': currentDay !== prize.day
+                           && prizes && (prizes.find(p => p.day === prize.day)).prize}"
+              >
+                <span>{{ prize.day }}</span>
+                <i v-if="prize.prize" class="fa-solid fa-circle-check is-pulled-right has-text-accent" />
+                <div style="max-height: 120px; overflow-y: hidden" class="has-text-centered">
+                  <img
+                    v-if="prize.prize"
+                    width="110"
+                    :src="require(`@/assets/img/festival/${prize.prize}.png`)"
+                  >
+                  <img
+                    v-else
+                    width="140"
+                    :src="require(`@/assets/img/festival/lootbox-${prize.type}.png`)"
+                  >
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="table-container">
-      <table class="table is-bordered">
-        <thead>
-          <tr>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
-            <th>Sunday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="week in 2" :key="week">
-            <td
-              v-for="prize in prizes.slice((week-1) * 7,week * 7)"
-              :key="prize.day"
-              :data-tooltip="
-                prize.info ? prize.info : null"
-              :class="{'is-active': currentDay === prize.day,
-                       'is-selected': currentDay !== prize.day
-                         && prizes && (prizes.find(p => p.day === prize.day)).prize}"
+    <!-- TODO: Winners block -->
+    <div v-if="winners" class="column is-2 winners">
+      <div class="mt-2 box has-border-accent has-background-light">
+        <h4 class="mb-4 is-family-sans-serif subtitle has-text-centered has-text-weight-bold" data-aos="fade-up">
+          Winners
+        </h4>
+        <ul>
+          <li class="is-flex py-1 is-justify-content-space-between is-size-7">
+            <span class="is-flex-grow-1">01 Nov.</span>
+            <span
+              style="max-width: 100px;"
+              class="has-text-accent blockchain-address"
             >
-              <span>{{ prize.day }}</span>
-              <div style="max-height: 120px; overflow-y: hidden" class="has-text-centered">
-                <img
-                  v-if="prize.prize"
-                  width="110"
-                  :src="require(`@/assets/img/festival/${prize.prize}.png`)"
-                >
-                <img
-                  v-else
-                  width="140"
-                  :src="require(`@/assets/img/festival/lootbox-${prize.type}.png`)"
-                >
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              xc73wdnewa23w2wswdrwe23jweWwd</span>
+          </li>
+          <li class="is-flex py-1 is-justify-content-space-between is-size-7">
+            <span class="is-flex-grow-1">01 Nov.</span>
+            <span
+              style="max-width: 100px;"
+              class="has-text-accent blockchain-address"
+            >
+              xc73wdnewa23w2wswdrwe23jweWwd</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -68,7 +101,8 @@ export default {
   data () {
     return {
       prizes: [],
-      currentDay: null
+      currentDay: null,
+      winners: false
     };
   },
   mounted () {
@@ -120,6 +154,20 @@ table {
   }
   tr:last-child td:last-child {
       border-bottom-right-radius: 10px;
+  }
+}
+.blockchain-address {
+  font-family: monospace;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  display: block;
+}
+
+.winners {
+  li {
+    border-bottom: 1px solid #303930;
   }
 }
 </style>
