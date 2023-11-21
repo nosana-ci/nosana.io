4bd0c7436b88622a8536a69d8bba44997d4a6bd7
Jairzijp <jairzijp123@gmail.com>
2023-11-21 13:51:51 +0100

    add test grid page

---
+++ components/TestGridPhases.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="roadmap-container">
+    <div class="roadmap mb-6">
+      <div class="roadmap-item is-completed box">
+        <div class="content has-radius-small p-3" data-aos="fade-up">
+          <div class="title mb-0 has-text-centered">
+            Phase 1
+          </div>
+          <div class="px-5">
+            <h2 class="subtitle is-3 has-text-centered">
+              Private Beta
+            </h2>
+            <ul>
+              <li>
+                Open-source software projects can use Nosana’s engine to run CI jobs for free.
+              </li>
+              <li>
+                Projects can securely store their secrets to a Secret Manager
+                (hosted for free by Nosana, or you can host your own) and specify per job which secrets
+                the node running the job can retrieve.
+              </li>
+              <li>
+                The beta program runs on verified nodes, to make sure your tests run smoothly.
+              </li>
+            </ul>
+            <p class="block has-text-centered">
+              <a
+                href="https://app.nosana.io"
+                class="button is-accent is-outlined has-text-weight-semibold"
+                target="_blank"
+              >Get Started</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="roadmap-item box mt-6">
+        <div class="content has-radius-small p-3" data-aos="fade-up">
+          <div class="title mb-0 has-text-centered">
+            Phase 2
+          </div>
+          <div class="px-5">
+            <h2 class="subtitle is-3 has-text-centered">
+              Mainnet Release
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.roadmap {
+  &:before {
+    mask-image: linear-gradient(180deg, transparent 0%, rgba(0, 0, 0, 1) 13%, rgba(0, 0, 0, 1) 48%, transparent 98%);
+    content: "";
+    position: absolute;
+    width: 6px;
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    top: -9px;
+    background: $accent;
+  }
+}
+.roadmap-item {
+  background: #010C04;
+  position: relative;
+  padding-bottom: 60px;
+  margin-top: 5rem !important;
+  &:last-child {
+    .title, h2, ul {
+      mask-image: linear-gradient(to bottom, black 0%, transparent 100%);
+    }
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0; right: 0; bottom: 0; left: 0;
+    z-index: -1;
+    margin: -1px;
+    border-radius: inherit;
+    background: linear-gradient(to bottom, $accent, #051707);
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    $size: 17px;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    top: -9px;
+    height: $size;
+    width: $size;
+    border-radius: 100%;
+    background: $dark-mode-background;
+    border: 2px solid $accent;
+  }
+  ul {
+    line-height: 25px;
+    margin: 20px;
+    font-weight: 300;
+    li {
+      margin-top: 17px;
+    }
+  }
+  .content {
+    max-width: 100%;
+    padding-left: 20px;
+    position: relative;
+    .title {
+      font-size: 48px;
+      color: $accent;
+      padding-bottom: 10px;
+      font-weight: 600;
+      line-height: 1.4;
+    }
+  }
+  @-webkit-keyframes glow-box {
+    from {
+      box-shadow: 0 0 7px 0 rgba($accent, 0.4);
+    }
+    to {
+      box-shadow: 0 0 17px 8px rgba($accent, 0.6);
+    }
+  }
+  &.is-active {
+    &:after {
+      border-color: $accent;
+      background: $accent;
+      animation: glow-box 1s ease-in-out infinite alternate;
+    }
+    ul li:before {
+      color: $accent;
+    }
+  }
+}
+@media screen and (max-width: 700px) {
+  .roadmap-item {
+    .content {
+      &:after {
+        width: 100%;
+      }
+      .px-5 {
+        padding: 0 !important;
+      }
+    }
+  }
+}
+</style>
---
+++ pages/test-grid.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title site-title is-spaced has-limited-width mt-6" style="width: 800px">
+          Test Grid
+        </h1>
+        <div class="columns mt-5">
+          <div class="column is-7" data-aos="fade-up">
+            <h2 class="subtitle">
+              Expand your reach with Nosana
+            </h2>
+            <div class="has-limited-width-small mt-4">
+              <p class="block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae rem, ducimus laborum maxime consectetur.
+              </p>
+              <p class="block">
+                <a
+                  href="#"
+                  class="mr-3 button is-accent is-wide"
+                  disabled
+                >
+                  <b>Registrations opens 10-12-2023</b>
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="column has-text-centered mb-6">
+            <div>
+              <img src="~assets/img/partners/technology.svg">
+            </div>
+          </div>
+        </div>
+        <div class="mt-6 py-6 has-text-centered has-background-gradient">
+          <h2 class="title mb-6" data-aos="fade-right">
+            Requirements
+          </h2>
+          <p class="has-limited-width-small is-horizontal-centered">
+            During the first phase of the Test Grid you will require one of the following GPUs:
+          </p>
+          <div class="table-container mt-3">
+            <table class="typography table is-horizontal-centered">
+              <tbody>
+                <tr data-aos="fade-in" data-aos-delay="100">
+                  <td class="py-2">
+                    NVIDIA GTX 4090
+                  </td>
+                </tr>
+                <tr data-aos="fade-in" data-aos-delay="200">
+                  <td class="py-2">
+                    NVIDA GTX 4080
+                  </td>
+                </tr>
+                <tr data-aos="fade-in" data-aos-delay="300">
+                  <td class="py-2">
+                    NVIDIA GTX 4070Ti
+                  </td>
+                </tr>
+                <tr data-aos="fade-in" data-aos-delay="400">
+                  <td class="py-2">
+                    NVIDIA GTX 4070
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="column is-8 is-horizontal-centered">
+        <h2 class="title mb-6 has-text-centered">
+          FAQ
+        </h2>
+        <accordion>
+          <accordion-item v-for="faq in faqs" :key="faq.question">
+            <!-- This slot will handle the title/header of the accordion and is the part you click on -->
+            <template slot="accordion-trigger">
+              <h3 class="subtitle mb-4 has-text-weight-semibold">
+                {{ faq.question }}
+              </h3>
+            </template>
+            <!-- This slot will handle all the content that is passed to the accordion -->
+            <template slot="accordion-content">
+              <!-- eslint-disable vue/no-v-html -->
+              <span v-html="faq.answer" />
+            </template>
+          </accordion-item>
+        </accordion>
+      </div>
+    </section>
+    <section class="section mt-5">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-6 is-offset-3">
+            <test-grid-phases />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  colorMode: 'dark',
+  data () {
+    return {
+      faqs: [
+        { question: 'How do I stake my NOS tokens?', answer: '- Get started by going to: <a href="https://app.nosana.io/stake/" target="_blank">https://app.nosana.io/stake/</a><br> - Connect your Solana Wallet<br> - Define how much you want to stake and the duration of your unstake period <br>- Sign the transaction<br><br> Now you are all set! You can claim the staking rewards anytime you want.' },
+        { question: 'Are my staked NOS tokens safe?', answer: 'Your NOS tokens are safe in your smart contract vault on the Solana Blockchain. The Nosana Staking Program is open source and audited, so nobody can access your staked tokens except for you! You can see your staked tokens on the Solana Blockchain.' },
+        { question: 'What is the APY?', answer: 'The APY is dynamic, the more $NOS is staked in total, the lower the APY will be and vice versa. You can find the current APY at: <a href="https://app.nosana.io/stake/" target="_blank">https://app.nosana.io/stake/</a>' }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "bulma/sass/utilities/mixins";
+.area {
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+}
+@include touch {
+  .area {
+    position: relative;
+    width: 100%;
+  }
+}
+</style>
