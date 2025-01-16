86fc71c1a001bfa7934cdf4fddac9939224a11b4
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-01-16 05:19:54 +0000

    fix spelling depreciated

--- static/register.sh
+++ static/register.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "\033[31mThis script is now deprecated: Please use \033[1;3;33mbash <(wget -qO- https://nosana.com/start.sh)\033[0;31m instead.\033[0m"
+echo -e "\033[31mThis script is now depreciated: Please use \033[1;3;33mbash <(wget -qO- https://nosana.com/start.sh)\033[0;31m instead.\033[0m"
